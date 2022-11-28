9495e46ee5af98d547b9e7e1ead6689f1a6dad05
SIVAGAYATHRI-1 <sivagayathri@recruitcrm.io>
2022-11-28 20:44:34 +0530

    pojo

---
+++ pojo_pages.java
@@ -0,0 +1,24 @@
+public class pojo_class {
+	private String email;
+	private String password;
+	public  String getEmail() {
+
+		return email;
+	}
+	public void setEmail(String mail1) {
+		this.email = mail1;
+	}
+
+	public String getPassword() {
+
+		return password;
+
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+
+
+}
