c67a398ad2629e1ba9e857667ce1a385fe69c5bf
SIVAGAYATHRI-1 <sivagayathri@recruitcrm.io>
2022-11-29 13:24:01 +0530

    signup_pojo

---
+++ signuppojo.java
@@ -0,0 +1,32 @@
+
+public class pojo_signup {
+	private String name;
+	private String signup_mail;
+	private String signup_password;
+	private String company ;
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSignup_mail() {
+		return signup_mail;
+	}
+	public void setSignup_mail(String signup_mail) {
+		this.signup_mail = signup_mail;
+	}
+	public String getSignup_password() {
+		return signup_password;
+	}
+	public void setSignup_password(String signup_password) {
+		this.signup_password = signup_password;
+	}
+	
+}
