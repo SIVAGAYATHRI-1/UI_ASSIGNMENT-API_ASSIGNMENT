0c60eb974933b12033bac5a6e1031bc7be09ed51
SIVAGAYATHRI-1 <sivagayathri@recruitcrm.io>
2023-01-24 19:46:54 +0530

    basetest

---
+++ BaseTest.java
@@ -0,0 +1,15 @@
+package com.practice.Framework.Base;
+
+import org.testng.annotations.BeforeClass;
+
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class BaseTest {
+	
+	public static RequestSpecification requestSpecification ;
+	public static Response response;
+	public static String id="177656";
+	
+
+}
